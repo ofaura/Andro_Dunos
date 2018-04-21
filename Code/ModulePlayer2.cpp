7f51f0b2fbcff28b9f3f862b2ecb9259f65c55f8
sherzock <jacobo.galofre@gmail.com>
2018-04-21 20:14:59 +0200

    Update Screen limits for player

--- Code/ModulePlayer2.cpp
+++ Code/ModulePlayer2.cpp
@@ -233,9 +233,9 @@ update_status ModulePlayer2::Update()
 
 		//y lim
 
-		if (position.y <= abs(App->render->camera.y) / SCREEN_SIZE +35)
+		if (position.y <= abs(App->render->camera.y) / SCREEN_SIZE)
 		{
-			position.y = 1 + abs(App->render->camera.y) / SCREEN_SIZE +35;
+			position.y = 1 + abs(App->render->camera.y) / SCREEN_SIZE;
 		}
 		else if (position.y >= (abs(App->render->camera.y) / SCREEN_SIZE) + SCREEN_HEIGHT - 17) // the -50 just showcases that the camera limit on the lower end works, it stopps player before they touch the ground and crash
 		{
