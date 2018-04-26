f6dd491e624ce062562203145c5461fd65304ee7
DLorenzoLaguno17 <36265574+DLorenzoLaguno17@users.noreply.github.com>
2018-04-26 13:42:38 +0200

    Added an enumeration for the buttons

--- Code/ModuleInput.h
+++ Code/ModuleInput.h
@@ -7,7 +7,7 @@
 #include "SDL\include\SDL.h"
 
 #define MAX_KEYS 300
-
+#define MAX_BUTTONS 100
 
 enum KEY_STATE
 {
@@ -15,6 +15,14 @@ enum KEY_STATE
 	KEY_DOWN,
 	KEY_REPEAT,
 	KEY_UP
+};
+
+enum BUTTON_STATE
+{
+	BUTTON_IDLE = 0,
+	BUTTON_DOWN,
+	BUTTON_REPEAT,
+	BUTTON_UP
 };
 
 class ModuleInput : public Module
@@ -30,6 +38,7 @@ public:
 
 public:
 	KEY_STATE keyboard[MAX_KEYS];
+	BUTTON_STATE gamepad[MAX_BUTTONS];
 	SDL_Event e;
 };
 
