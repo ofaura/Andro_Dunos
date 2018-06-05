e4fefe4a2291e24f655146d84a8cb3de83306fab
GAPIntoTheGame <36154978+GAPIntoTheGame@users.noreply.github.com>
2018-06-05 23:42:24 +0200

    done

--- Code/ModuleSceneLevel5.cpp
+++ Code/ModuleSceneLevel5.cpp
@@ -128,12 +128,13 @@ bool ModuleSceneLevel5::Start()
 	App->enemies->AddEnemy(ENEMY_TYPES::GREEN_BALL, 500, (SCREEN_HEIGHT / 2) - 20);
 
 	App->enemies->AddEnemy(ENEMY_TYPES::ENEMY_POWERUP, 300, (SCREEN_HEIGHT / 2 - 20));
-	App->enemies->AddEnemy(ENEMY_TYPES::ENEMY_POWERUP, 400, (SCREEN_HEIGHT / 2) - 20);
-	App->enemies->AddEnemy(ENEMY_TYPES::ENEMY_POWERUP, 500, (SCREEN_HEIGHT / 2) - 20);
-	App->enemies->AddEnemy(ENEMY_TYPES::ENEMY_POWERUP, 600, (SCREEN_HEIGHT / 2) - 20);
-	App->enemies->AddEnemy(ENEMY_TYPES::ENEMY_POWERUP, 700, (SCREEN_HEIGHT / 2) - 20);
-	App->enemies->AddEnemy(ENEMY_TYPES::ENEMY_POWERUP, 800, (SCREEN_HEIGHT / 2) - 20);
-	App->enemies->AddEnemy(ENEMY_TYPES::ENEMY_POWERUP, 900, (SCREEN_HEIGHT / 2) - 20);
+	App->enemies->AddEnemy(ENEMY_TYPES::ENEMY_POWERUP, 1175, (SCREEN_HEIGHT / 2) - 20);
+	App->enemies->AddEnemy(ENEMY_TYPES::ENEMY_POWERUP, 2060, (SCREEN_HEIGHT / 2) - 20);
+	App->enemies->AddEnemy(ENEMY_TYPES::ENEMY_POWERUP, 2925, (SCREEN_HEIGHT / 2) - 20);
+	App->enemies->AddEnemy(ENEMY_TYPES::ENEMY_POWERUP, 3800, (SCREEN_HEIGHT / 2) - 20);
+	App->enemies->AddEnemy(ENEMY_TYPES::ENEMY_POWERUP, 4675, (SCREEN_HEIGHT / 2) - 20);
+	App->enemies->AddEnemy(ENEMY_TYPES::ENEMY_POWERUP, 5550, (SCREEN_HEIGHT / 2) - 20);
+	App->enemies->AddEnemy(ENEMY_TYPES::ENEMY_POWERUP, 6425, (SCREEN_HEIGHT / 2) - 20);
 
 	// Mines
 	App->enemies->AddEnemy(ENEMY_TYPES::ENEMY_MINE, 760, 120);
